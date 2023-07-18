c88d457c02ea174273e43552e808411d7fff9980
mucrow <120820315+mucrow@users.noreply.github.com>
2023-07-18 09:53:09 -0400

    Create git repository

---
+++ Assets/Scripts/RecordingButton.cs
@@ -0,0 +1,62 @@
+﻿using System.IO;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Diamond {
+  public class RecordingButton: MonoBehaviour {
+    [SerializeField] int _maxRecordingLength = 60;
+
+    AudioSource _audioSource;
+    Button _button;
+    Image _image;
+    bool _isRecording = false;
+    AudioClip _microphoneAudioClip;
+    Color _startColor;
+    TMP_Text _text;
+
+    void Awake() {
+      _audioSource = GetComponent<AudioSource>();
+      _button = GetComponent<Button>();
+      _button.onClick.AddListener(OnClick);
+      _image = GetComponent<Image>();
+      _text = GetComponentInChildren<TMP_Text>();
+      _startColor = _image.color;
+    }
+
+    public void OnClick() {
+      if (_isRecording) {
+        StopRecording();
+      }
+      else {
+        StartRecording();
+      }
+    }
+
+    void StartRecording() {
+      _image.color = new Color(1f, 0.2f, 0.2f);
+      _text.text = "Stop Recording";
+
+      _audioSource.Stop();
+      string microphoneName = Microphone.devices[0];
+      _microphoneAudioClip = Microphone.Start(microphoneName, false, _maxRecordingLength, AudioSettings.outputSampleRate);
+
+      _isRecording = true;
+    }
+
+    void StopRecording() {
+      _image.color = _startColor;
+      _text.text = "Start Recording";
+
+      string microphoneName = Microphone.devices[0];
+      Microphone.End(microphoneName);
+      // _audioSource.clip = _microphoneAudioClip;
+      // _audioSource.Play();
+
+      var savePath = Path.Combine(Application.dataPath, System.DateTime.Now.ToString("yyyyMMddHHmmss") + ".wav");
+      SavWav.Save(savePath, _microphoneAudioClip);
+
+      _isRecording = false;
+    }
+  }
+}
---
+++ Assets/Scripts/ScaleWithLoudness.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Diamond {
+  public class ScaleWithLoudness: MonoBehaviour {
+    [SerializeField] float _loudnessCoeff = 5f;
+    [SerializeField] float _minScale = 0.25f;
+    [SerializeField] float _releaseTime = 0.05f;
+    [SerializeField] bool _useMicrophone;
+
+    float _scale = 1f;
+
+    AudioClip _microphoneAudioClip;
+    AudioSource _audioSource;
+
+    void Awake() {
+      _audioSource = GetComponent<AudioSource>();
+      if (_useMicrophone) {
+        string microphoneName = Microphone.devices[0];
+        _microphoneAudioClip = Microphone.Start(microphoneName, true, 5 * 60, AudioSettings.outputSampleRate);
+        _audioSource.clip = _microphoneAudioClip;
+        _audioSource.loop = true;
+        _audioSource.Play();
+      }
+    }
+
+    void Update() {
+      float loudnessRange = _loudnessCoeff - _minScale;
+
+      if (_scale > _minScale) {
+        _scale -= loudnessRange * (Time.deltaTime / _releaseTime);
+        _scale = Mathf.Max(_scale, _minScale);
+      }
+
+      float loudness = GetLoudness();
+      var targetScale = loudness * _loudnessCoeff;
+      _scale = Mathf.Max(targetScale, _scale, _minScale);
+      transform.localScale = new Vector3(1f, 1f, 1f) * _scale;
+    }
+
+    float GetLoudness() {
+      if (_useMicrophone) {
+        return GetLoudnessFromMicrophone();
+      }
+      return GetLoudnessFromAudioClip(_audioSource.timeSamples, _audioSource.clip);
+    }
+
+    float GetLoudnessFromMicrophone() {
+      return GetLoudnessFromAudioClip(Microphone.GetPosition(Microphone.devices[0]), _microphoneAudioClip);
+    }
+
+    static float GetLoudnessFromAudioClip(int clipPosition, AudioClip clip) {
+      int sampleWindow = 60;
+      int startPosition = clipPosition - sampleWindow;
+
+      if (startPosition < 0) {
+        return 0f;
+      }
+
+      float[] waveData = new float[sampleWindow];
+      clip.GetData(waveData, startPosition);
+
+      float totalLoudness = 0f;
+      for (int i = 0; i < sampleWindow; ++i) {
+        totalLoudness += Mathf.Abs(waveData[i]);
+      }
+
+      return totalLoudness / sampleWindow;
+    }
+  }
+}
